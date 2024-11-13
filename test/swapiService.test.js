7bfb55992aacc17a06d59ad2b2026f655d71ecfd
joaozinSHSN <joao.l.cruz@iesb.edu.br>
2024-11-13 09:23:49 -0300

    1º Teste

--- test/swapiService.test.js
+++ test/swapiService.test.js
@@ -27,58 +27,68 @@ const getNonexistentPlanet = () => fetchData('planets/999999/');
 
 // Testes utilizando a API real
 describe('SWAPI Tests', () => {
+  // Teste1
   test('should fetch planets', async () => {
     const data = await getPlanets();
     expect(data.count).toBeGreaterThan(0);
     expect(data.results).toBeInstanceOf(Array);
   });
 
-  test('should fetch starships', async () => {
-    const data = await getStarships();
-    expect(data.count).toBeGreaterThan(0);
-    expect(data.results).toBeInstanceOf(Array);
-  });
+  // // Teste2
+  // test('should fetch starships', async () => {
+  //   const data = await getStarships();
+  //   expect(data.count).toBeGreaterThan(0);
+  //   expect(data.results).toBeInstanceOf(Array);
+  // });
 
-  test('should fetch films', async () => {
-    const data = await getFilms();
-    expect(data.count).toBeGreaterThan(0);
-    expect(data.results).toBeInstanceOf(Array);
-  });
+  // // Teste3
+  // test('should fetch films', async () => {
+  //   const data = await getFilms();
+  //   expect(data.count).toBeGreaterThan(0);
+  //   expect(data.results).toBeInstanceOf(Array);
+  // });
 
-  test('should return 404 for an unknown route', async () => {
-    const result = await getUnknownRoute();
-    expect(result.status).toBe(404);
-  });
+  // // Teste4
+  // test('should return 404 for an unknown route', async () => {
+  //   const result = await getUnknownRoute();
+  //   expect(result.status).toBe(404);
+  // });
 
-  test('should fetch planet by id', async () => {
-    const data = await getPlanetById(1);
-    expect(data.name).toBe('Tatooine');
-  });
+  // // Teste5
+  // test('should fetch planet by id', async () => {
+  //   const data = await getPlanetById(1);
+  //   expect(data.name).toBe('Tatooine');
+  // });
 
-  test('should fetch all people', async () => {
-    const data = await getPeople();
-    expect(data.count).toBeGreaterThan(0);
-    expect(data.results).toBeInstanceOf(Array);
-  });
+  // // Teste6
+  // test('should fetch all people', async () => {
+  //   const data = await getPeople();
+  //   expect(data.count).toBeGreaterThan(0);
+  //   expect(data.results).toBeInstanceOf(Array);
+  // });
 
-  test('should fetch all species', async () => {
-    const data = await getSpecies();
-    expect(data.count).toBeGreaterThan(0);
-    expect(data.results).toBeInstanceOf(Array);
-  });
+  // // Teste7
+  // test('should fetch all species', async () => {
+  //   const data = await getSpecies();
+  //   expect(data.count).toBeGreaterThan(0);
+  //   expect(data.results).toBeInstanceOf(Array);
+  // });
 
-  test('should fetch a starship by ID', async () => {
-    const data = await getStarshipById(9);
-    expect(data.name).toBe('Death Star');
-  });
+  // // Teste8
+  // test('should fetch a starship by ID', async () => {
+  //   const data = await getStarshipById(9);
+  //   expect(data.name).toBe('Death Star');
+  // });
 
-  test('should fetch a film by ID', async () => {
-    const data = await getFilmById(1);
-    expect(data.title).toBe('A New Hope');
-  });
+  // // Teste9
+  // test('should fetch a film by ID', async () => {
+  //   const data = await getFilmById(1);
+  //   expect(data.title).toBe('A New Hope');
+  // });
 
-  test('should return 404 for a nonexistent planet', async () => {
-    const result = await getNonexistentPlanet();
-    expect(result.status).toBe(404);
-  });
+  // // Teste10
+  // test('should return 404 for a nonexistent planet', async () => {
+  //   const result = await getNonexistentPlanet();
+  //   expect(result.status).toBe(404);
+  // });
 });
